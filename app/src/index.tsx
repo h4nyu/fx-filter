cba4c8900fb65f8cae2e09a788cc53448badf5e0
h4nyu <yao.ntno@gmail.com>
2020-02-15 03:03:09 +0000

    add env

---
+++ app/src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+render(<div>bbb</div>, document.getElementById('app'));
---
+++ app/webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  watchOptions: {
+    aggregateTimeout: 600,
+    poll: 1000
+  },
+  entry: './src/index.tsx',
+  devServer: {
+    host: "0.0.0.0",
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./templates/index.html",
+    }),
+  ],
+  output:{
+    filename: "[hash].js"
+  },
+  optimization:{
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader"
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'file-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[hash:7].[ext]'
+        },
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      '~': '/srv/src',
+      'stories': '/srv/stories',
+      'tests': '/srv/tests',
+      'package.json':'/srv/package.json',
+    },
+    extensions: ['.tsx', '.ts', '.js' ]
+  }
+};
