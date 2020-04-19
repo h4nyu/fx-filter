bc7a7ae2aa7e287b95a367d23e887eca69d5a1d7
908kre <ce51046.higuchi@gmail.com>
2020-04-19 09:23:48 +0000

    fix Segment

--- src/entities.ts
+++ src/entities.ts
@@ -1,5 +1,6 @@
 import {Moment} from 'moment';
 import {Map} from 'immutable';
+
 export type OandaAccount = {
   id: string,
   tags: string[],
@@ -24,13 +25,16 @@ export type Candle = {
 export type Candles = Candle[];
 
 export type Segment = {
+  id: string,
   currencyPair: CurrencyPair,
-  candles: Candles,
-  count: number,
-  upRatio: number,
-  downRatio: number,
+  granularity: Granularity,
+  direction: Direction,
+  fromDate: Moment,
+  toDate: Moment,
+  ratio: number,
 }
-export type Segments = Map<CurrencyPair, Segment>;
+
+export type Segments = Map<string, Segment>;
 
 export type Instruments = Instrument[];
 
@@ -39,8 +43,14 @@ export enum Granularity {
   H1 = "H1",
   D = "D",
 }
+
 export enum CurrencyPair {
   EUR_JPY = "EUR_JPY",
   USD_JPY = "USD_JPY",
   AUD_JPY = "AUD_JPY",
+}
+
+export enum Direction {
+  High = "High",
+  Low = "Low",
 }
--- stories/components/Segment.tsx
+++ stories/components/Segment.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import moment from 'moment';
 import { storiesOf } from '@storybook/react';
 import Component from '~/components/Segment';
-import {CurrencyPair} from '~/entities';
+import {CurrencyPair, Direction, Granularity} from '~/entities';
 import { object } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 storiesOf('Segment', module)
   .add('default', () => {
     const segment = {
+      id: 'aaa',
       currencyPair: CurrencyPair.USD_JPY,
-      candles: [],
-      count: 1000,
-      upRatio:0.3,
-      downRatio:0.7,
+      granularity: Granularity.H1,
+      direction: Direction.High,
+      fromDate: moment(),
+      toDate: moment().add(1, 'days'),
+      ratio:0.7,
     }
     return (<Component
       row={object('row', segment)}
     />)
   })
-
