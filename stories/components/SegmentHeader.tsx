9337eabe6aee66d5814a3525638db5368db24180
908kre <ce51046.higuchi@gmail.com>
2020-04-19 10:23:16 +0000

    aaa

---
+++ stories/components/SegmentHeader.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import moment from 'moment';
+import { storiesOf } from '@storybook/react';
+import Component from '~/components/SegmentHeader';
+import Segment from '~/components/Segment';
+import {CurrencyPair, Direction, Granularity} from '~/entities';
+import { object } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+storiesOf('SegmentHeader', module)
+  .add('default', () => {
+    const segment = {
+      id: 'aaa',
+      currencyPair: CurrencyPair.USD_JPY,
+      granularity: Granularity.H1,
+      direction: Direction.High,
+      fromDate: moment(),
+      toDate: moment().add(1, 'days'),
+      ratio:0.7,
+    }
+    return (
+      <>
+        <Component
+          onClear={action('clear')}
+        />
+        <Segment
+          row={segment}
+        />
+      </>
+    )
+  })
